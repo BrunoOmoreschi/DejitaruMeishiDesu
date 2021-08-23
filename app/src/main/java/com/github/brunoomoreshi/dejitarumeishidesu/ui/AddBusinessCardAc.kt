05fef8c6728d35545af669eeca84dce3215bed68
BrunoOmoreschi <brunomoreschi.camposdesouza@gmail.com>
2021-08-23 20:42:24 -0300

    Progresso no layout
    Quase pronto! - O layout né ....

---
+++ app/src/main/java/com/github/brunoomoreshi/dejitarumeishidesu/ui/AddBusinessCardAc.kt
@@ -0,0 +1,11 @@
+package com.github.brunoomoreshi.dejitarumeishidesu.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBusinessCardAc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_business_card)
+    }
+}
